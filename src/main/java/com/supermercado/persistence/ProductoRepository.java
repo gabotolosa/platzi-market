77df0370e9c8e9fc357c07e6195cef86019f4e6a
Jesus Gabriel Tolosa Z <ing.tolosa@gmail.com>
2023-02-07 09:25:57 -0500

    implementar la anotacion service

--- src/main/java/com/supermercado/persistence/ProductoRepository.java
+++ src/main/java/com/supermercado/persistence/ProductoRepository.java
@@ -6,6 +6,7 @@ import com.supermercado.domain.repository.ProductRepository;
 import com.supermercado.persistence.crud.ProductoCrudRepository;
 import com.supermercado.persistence.entity.Producto;
 import com.supermercado.persistence.mapper.ProductMapper;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -14,7 +15,10 @@ import java.util.Optional;
 @Repository
 public class ProductoRepository implements ProductRepository {
 
+    @Autowired
     private ProductoCrudRepository productoCrudRepository;
+
+    @Autowired
     private ProductMapper mapper;
 
     @Override
