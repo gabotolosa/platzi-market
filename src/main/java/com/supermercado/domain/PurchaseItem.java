6e8134e054c592389cc5c9fb4639cfdf7ebb548e
Jesus Gabriel Tolosa Z <ing.tolosa@gmail.com>
2023-02-08 08:52:14 -0500

    creando dominio d compras purchase

---
+++ src/main/java/com/supermercado/domain/PurchaseItem.java
@@ -0,0 +1,43 @@
+package com.supermercado.domain;
+
+import java.math.BigDecimal;
+
+public class PurchaseItem {
+
+    private Long productId;
+    private Long quantity;
+    private BigDecimal total;
+    private Boolean active;
+
+    public Long getProductId() {
+        return productId;
+    }
+
+    public void setProductId(Long productId) {
+        this.productId = productId;
+    }
+
+    public Long getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(Long quantity) {
+        this.quantity = quantity;
+    }
+
+    public BigDecimal getTotal() {
+        return total;
+    }
+
+    public void setTotal(BigDecimal total) {
+        this.total = total;
+    }
+
+    public Boolean getActive() {
+        return active;
+    }
+
+    public void setActive(Boolean active) {
+        this.active = active;
+    }
+}
---
+++ src/main/java/com/supermercado/domain/repository/PurchaseRepository.java
@@ -0,0 +1,12 @@
+package com.supermercado.domain.repository;
+
+import com.supermercado.domain.Purchase;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface PurchaseRepository {
+    List<Purchase> getAll();
+    Optional<List<Purchase>> getByClient(String clientId);
+    Purchase save(Purchase purchase);
+}
